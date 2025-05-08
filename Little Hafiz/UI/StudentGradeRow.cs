ced87ccf0b3dc90435ca7e6c4a7109b96d9382f9
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-05-08 14:57:19 +0300

    fix error

--- Little Hafiz/UI/StudentGradeRow.cs
+++ Little Hafiz/UI/StudentGradeRow.cs
@@ -40,7 +40,7 @@ namespace Little_Hafiz
 
         private void CompDate_DoubleClick(object sender, EventArgs e)
         {
-            if (CompetitionGradeData.Notes != "")
+            if (CompetitionGradeData != null && CompetitionGradeData.Notes != "")
                 MessageBox.Show(CompetitionGradeData.Notes);
         }
         
