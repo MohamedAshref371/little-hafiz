927cf667a7bbbf536b887a40206604576d07e5b6
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-05-10 15:28:17 +0300

    v1.92

--- Little Hafiz/Properties/AssemblyInfo.cs
+++ Little Hafiz/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("برنامج مسابقات القرآن الكريم")]
+[assembly: AssemblyTitle("Qur'an Recitation Competition Program")]
 [assembly: AssemblyDescription("github.com/MohamedAshref371")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("github.com/MohamedAshref371")]
-[assembly: AssemblyProduct("Qur'an Recitation Competition Program")]
+[assembly: AssemblyProduct("برنامج مسابقات القرآن الكريم")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.91.0.0")]
-[assembly: AssemblyFileVersion("1.91.0.0")]
+[assembly: AssemblyVersion("1.92.0.0")]
+[assembly: AssemblyFileVersion("1.92.0.0")]
