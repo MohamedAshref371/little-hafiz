2a9dd632653a7926c16229d847fc8ea490e864a7
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-05-11 21:32:41 +0300

    slip

--- Little Hafiz/UI/Form1.Designer.cs
+++ Little Hafiz/UI/Form1.Designer.cs
@@ -3322,7 +3322,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "مكتب الحافظ الصغير بمسطرد";
+            this.Text = "برنامج مسابقات القرآن الكريم";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.studentDataPanel.ResumeLayout(false);
             this.studentDataPanel.PerformLayout();
